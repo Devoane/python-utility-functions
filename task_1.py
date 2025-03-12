655c39586e88981c22dcc9ab676a3993929bce69
Devoane <162355799+Devoane@users.noreply.github.com>
2025-03-12 07:07:53 +0100

    update

--- task_1.py
+++ task_1.py
@@ -1,3 +1,14 @@
-import math
-import random
-import time
+import math         # https://docs.python.org/3/library/math.html
+import random       # https://docs.python.org/3/library/random.htm
+import time         # https://docs.python.org/3/library/time.html
+
+def generate_random_data(size=5):
+    """Generates random numerical and textual data."""
+    numbers = [random.randint(1, 100) for _ in range(size)]
+    letters = [chr(random.randint(65, 90)) for _ in range(size)]
+    return numbers, letters
+
+def zipped(list1, list2):
+    """Zips two lists together."""
+    return list(zip(list1, list2))    # https://docs.python.org/3/library/functions.html#zip
+
