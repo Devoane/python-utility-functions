5990661fa0828789e80d3bd28b63f3a8ee9299e0
Devoane <162355799+Devoane@users.noreply.github.com>
2025-03-24 14:17:28 +0100

    update

--- Library(Task3)/my_lib/__init__.py
+++ Library(Task3)/my_lib/__init__.py
@@ -2,7 +2,7 @@
 My Library - A Python utility library for data processing, mathematical operations, and text manipulation.
 """
 
-from .data_utils import remove_duplicates, flatten_list
+from .data_utils import convert_dicts_to_json, parse_json
 from .math_tools import factorial, gcd
 from .text_processing import count_vowels, reverse_words
 
